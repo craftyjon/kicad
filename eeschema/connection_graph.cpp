1939842393b433fd7359d58d5455cc6302b6eb1e
Jon Evans <jon@craftyjon.com>
2017-11-23 09:06:56 -0500

    Add connection_graph.cpp

---
+++ eeschema/connection_graph.cpp
@@ -0,0 +1,49 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2017 CERN
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <connection_graph.h>
+
+
+void CONNECTION_VISITOR::tree_edge( const CONNECTION_GRAPH_T::edge_descriptor aEdge,
+                                    const CONNECTION_GRAPH_T& aGraph ) const
+{
+    // auto source_item = aGraph[ boost::source( aEdge, aGraph ) ].item;
+    auto target_item = aGraph[ boost::target( aEdge, aGraph ) ].item;
+
+    // std::cout << "source " << source_item->m_connection->m_name
+    //           << " isbus " << source_item->m_connection->IsBus()
+    //           << " isnet " << source_item->m_connection->IsNet() << std::endl;
+    // std::cout << "target " << target_item->m_connection->m_name
+    //           << " isbus " << target_item->m_connection->IsBus()
+    //           << " isnet " << target_item->m_connection->IsNet() << std::endl;
+
+    // don't propagate across bus/net boundary
+    if( ( m_connection.IsBus() && target_item->m_connection->IsNet() ) ||
+        ( m_connection.IsNet() && target_item->m_connection->IsBus() ) )
+    {
+        return;
+    }
+
+    if( target_item->m_connection_dirty )
+    {
+        target_item->m_connection = m_connection;
+        target_item->m_connection_dirty = false;
+    }
+}
