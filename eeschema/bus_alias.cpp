77f128bf687c49c1241eb14e2535e1b6ee6f55c3
Jon Evans <jon@craftyjon.com>
2018-01-01 22:28:48 -0500

    Add author tags

--- eeschema/bus_alias.cpp
+++ eeschema/bus_alias.cpp
@@ -2,6 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2017 CERN
+ * @author Jon Evans <jon@craftyjon.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
